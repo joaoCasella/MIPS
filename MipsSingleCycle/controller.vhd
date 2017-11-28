5be5e4bfefd51a99dab5bac7b4cb597e6aa3050f
jengaehassim <gianlucca.nicolosi@gmail.com>
2017-11-28 18:43:08 -0200

    Implementação do bne usando um outro sinal de branch (branchne), para diferenciar da branch comum

--- MipsSingleCycle/controller.vhd
+++ MipsSingleCycle/controller.vhd
@@ -13,24 +13,24 @@ end;
 
 architecture struct of controller is
   component maindec
-    port(op:                 in  STD_LOGIC_VECTOR(5 downto 0);
-         memtoreg, memwrite: out STD_LOGIC;
-         branch, alusrc:     out STD_LOGIC;
-         regdst, regwrite:   out STD_LOGIC;
-         jump:               out STD_LOGIC;
-         aluop:              out STD_LOGIC_VECTOR(1 downto 0));
+    port(op:                       in  STD_LOGIC_VECTOR(5 downto 0);
+         memtoreg, memwrite:       out STD_LOGIC;
+         branch, branchne, alusrc: out STD_LOGIC;
+         regdst, regwrite:         out STD_LOGIC;
+         jump:                     out STD_LOGIC;
+         aluop:                    out STD_LOGIC_VECTOR(1 downto 0));
   end component;
   component aludec
     port(funct:      in  STD_LOGIC_VECTOR(5 downto 0);
          aluop:      in  STD_LOGIC_VECTOR(1 downto 0);
          alucontrol: out STD_LOGIC_VECTOR(2 downto 0));
   end component;
-  signal aluop:  STD_LOGIC_VECTOR(1 downto 0);
-  signal branch: STD_LOGIC;
+  signal aluop:            STD_LOGIC_VECTOR(1 downto 0);
+  signal branch, branchne: STD_LOGIC;
 begin
-  md: maindec port map(op, memtoreg, memwrite, branch,
+  md: maindec port map(op, memtoreg, memwrite, branch, branchne,
                        alusrc, regdst, regwrite, jump, aluop);
   ad: aludec port map(funct, aluop, alucontrol);
 
-  pcsrc <= branch and zero;
+  pcsrc <= (branch and zero) or (branchne and (not zero));
 end;
--- MipsSingleCycle/maindec.vhd
+++ MipsSingleCycle/maindec.vhd
@@ -1,29 +1,30 @@
 library IEEE; use IEEE.STD_LOGIC_1164.all;
 
 entity maindec is -- main control decoder
-  port(op:                 in  STD_LOGIC_VECTOR(5 downto 0);
-       memtoreg, memwrite: out STD_LOGIC;
-       branch, alusrc:     out STD_LOGIC;
-       regdst, regwrite:   out STD_LOGIC;
-       jump:               out STD_LOGIC;
-       aluop:              out STD_LOGIC_VECTOR(1 downto 0));
+  port(op:                       in  STD_LOGIC_VECTOR(5 downto 0);
+       memtoreg, memwrite:       out STD_LOGIC;
+       branch, branchne, alusrc: out STD_LOGIC;
+       regdst, regwrite:         out STD_LOGIC;
+       jump:                     out STD_LOGIC;
+       aluop:                    out STD_LOGIC_VECTOR(1 downto 0));
 end;
 
 architecture behave of maindec is
-  signal controls: STD_LOGIC_VECTOR(8 downto 0);
+  signal controls: STD_LOGIC_VECTOR(9 downto 0);
 begin
   process(all) begin
     case op is
-      when "000000" => controls <= "110000010"; -- RTYPE
-      when "100011" => controls <= "101001000"; -- LW
-      when "101011" => controls <= "001010000"; -- SW
-      when "000100" => controls <= "000100001"; -- BEQ
-      when "001000" => controls <= "101000000"; -- ADDI
-      when "000010" => controls <= "000000100"; -- J
-      when others   => controls <= "---------"; -- illegal op
+      when "000000" => controls <= "1100000010"; -- RTYPE
+      when "100011" => controls <= "1010001000"; -- LW
+      when "101011" => controls <= "0010010000"; -- SW
+      when "000100" => controls <= "0001000001"; -- BEQ
+      when "000101" => controls <= "0000100001"; -- BNE
+      when "001000" => controls <= "1010000000"; -- ADDI
+      when "000010" => controls <= "0000000100"; -- J
+      when others   => controls <= "----------"; -- illegal op
     end case;
   end process;
 
-  (regwrite, regdst, alusrc, branch, memwrite,
+  (regwrite, regdst, alusrc, branch, branchne, memwrite,
    memtoreg, jump, aluop(1 downto 0)) <= controls;
 end;
