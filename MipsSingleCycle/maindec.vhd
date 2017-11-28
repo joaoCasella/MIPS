04b9bc4749524614f98d889d1aaeb6cde8e6d7fc
joaoCasella <jpedrocaselladepaula@hotmail.com>
2017-11-28 20:57:10 -0200

    Fixed the signal length

--- MipsSingleCycle/maindec.vhd
+++ MipsSingleCycle/maindec.vhd
@@ -11,7 +11,7 @@ entity maindec is -- main control decoder
 end;
 
 architecture behave of maindec is
-  signal controls: STD_LOGIC_VECTOR(8 downto 0);
+  signal controls: STD_LOGIC_VECTOR(9 downto 0);
 begin
   process(all) begin
     case op is
