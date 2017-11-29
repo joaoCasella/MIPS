71f2ea9ec33a013dfeed9798f3b1a5f490ddbcc3
joaoCasella <jpedrocaselladepaula@hotmail.com>
2017-11-29 03:45:33 -0200

    Created the first files to detect hazards - still in progress

---
+++ MipsPipeline/hazarddetection.vhd
@@ -0,0 +1,12 @@
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+
+entity hazarddetection is -- hazard detector
+  port(idexrt, ifidrs, ifidrt: in  STD_LOGIC_VECTOR(31 downto 0);
+       memread:                in  STD_LOGIC;
+       stall:                  out STD_LOGIC);
+end;
+
+architecture behave of hazarddetection is
+begin
+  stall <= memread and (idexrt = ifidrs or idexrt = ifidrt);
+end;
