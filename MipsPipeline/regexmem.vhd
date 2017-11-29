ab35a1822c920a1e40a362d243b4740995f3ff6c
jengaehassim <gianlucca.nicolosi@gmail.com>
2017-11-29 14:35:43 -0200

    Registrador entre execução e memoria

---
+++ MipsPipeline/regexmem.vhd
@@ -0,0 +1,28 @@
+library IEEE; use IEEE.STD_LOGIC_1164.all; 
+use IEEE.NUMERIC_STD_UNSIGNED.all;
+
+entity regexmem is -- register between ex and mem
+  port(clk:                                   in  STD_LOGIC;
+       zero_ex:                               in  STD_LOGIC;
+       pcjump_ex, aluout_ex, writedata_ex:    in  STD_LOGIC_VECTOR(31 downto 0);
+       zero_mem:                              out STD_LOGIC;
+       pcjump_mem, aluout_mem, writedata_mem: out STD_LOGIC_VECTOR(31 downto 0));
+end;
+
+architecture behave of regexmem is
+  signal zero: STD_LOGIC;
+  signal pcjump, aluout, writedata: STD_LOGIC_VECTOR(31 downto 0);
+begin
+  process(clk) begin
+    if rising_edge(clk) then
+      zero_mem      <= zero;
+      pcjump_mem    <= pcjump;
+      aluout_mem    <= aluout;
+      writedata_mem <= writedata;
+      zero          <= zero_ex;
+      pcjump        <= pcjump_ex;
+      aluout        <= aluout_ex;
+      writedata     <= writedata_ex;
+    end if;
+  end process;
+end;
--- MipsPipeline/regmemwb.vhd
+++ MipsPipeline/regmemwb.vhd
@@ -1,21 +1,21 @@
 library IEEE; use IEEE.STD_LOGIC_1164.all; 
 use IEEE.NUMERIC_STD_UNSIGNED.all;
 
-entity regmemwb is -- three-port register file
-  port(clk:                         in  STD_LOGIC;
-       readdata_mem, aluresult_mem: in  STD_LOGIC_VECTOR(31 downto 0);
-       readdata_wb, aluresult_wb:   out STD_LOGIC_VECTOR(31 downto 0));
+entity regmemwb is -- register between mem and wb
+  port(clk:                      in  STD_LOGIC;
+       readdata_mem, aluout_mem: in  STD_LOGIC_VECTOR(31 downto 0);
+       readdata_wb, aluout_wb:   out STD_LOGIC_VECTOR(31 downto 0));
 end;
 
 architecture behave of regmemwb is
-  signal readdata, aluresult: STD_LOGIC_VECTOR(31 downto 0);
+  signal readdata, aluout: STD_LOGIC_VECTOR(31 downto 0);
 begin
   process(clk) begin
     if rising_edge(clk) then
-      readdata_wb  <= readdata;
-      aluresult_wb <= aluresult;
-      readdata     <= readdata_mem;
-      aluresult    <= aluresult_mem;
+      readdata_wb <= readdata;
+      aluout_wb   <= aluout;
+      readdata    <= readdata_mem;
+      aluout      <= aluout_mem;
     end if;
   end process;
 end;
