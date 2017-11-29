df23036f2fe7e24cf6a19b247dff502483838ed8
jengaehassim <gianlucca.nicolosi@gmail.com>
2017-11-29 15:02:43 -0200

    Added writedata signals

--- MipsPipeline/regexmem.vhd
+++ MipsPipeline/regexmem.vhd
@@ -4,22 +4,27 @@ use IEEE.NUMERIC_STD_UNSIGNED.all;
 entity regexmem is -- register between ex and mem
   port(clk:                                   in  STD_LOGIC;
        zero_ex:                               in  STD_LOGIC;
+       writereg_ex:                           in  STD_LOGIC_VECTOR(4 downto 0);
        pcjump_ex, aluout_ex, writedata_ex:    in  STD_LOGIC_VECTOR(31 downto 0);
        zero_mem:                              out STD_LOGIC;
+       writereg_mem:                          out STD_LOGIC_VECTOR(4 downto 0);
        pcjump_mem, aluout_mem, writedata_mem: out STD_LOGIC_VECTOR(31 downto 0));
 end;
 
 architecture behave of regexmem is
-  signal zero: STD_LOGIC;
+  signal zero:                      STD_LOGIC;
+  signal writereg:                  STD_LOGIC_VECTOR(4 downto 0);
   signal pcjump, aluout, writedata: STD_LOGIC_VECTOR(31 downto 0);
 begin
   process(clk) begin
     if rising_edge(clk) then
       zero_mem      <= zero;
+      writereg_mem  <= writereg;
       pcjump_mem    <= pcjump;
       aluout_mem    <= aluout;
       writedata_mem <= writedata;
       zero          <= zero_ex;
+      writereg      <= writedata_ex;
       pcjump        <= pcjump_ex;
       aluout        <= aluout_ex;
       writedata     <= writedata_ex;
--- MipsPipeline/regmemwb.vhd
+++ MipsPipeline/regmemwb.vhd
@@ -3,17 +3,22 @@ use IEEE.NUMERIC_STD_UNSIGNED.all;
 
 entity regmemwb is -- register between mem and wb
   port(clk:                      in  STD_LOGIC;
+       writereg_mem:             in  STD_LOGIC_VECTOR(4 downto 0);
        readdata_mem, aluout_mem: in  STD_LOGIC_VECTOR(31 downto 0);
+       writereg_wb:              out STD_LOGIC_VECTOR(4 downto 0);
        readdata_wb, aluout_wb:   out STD_LOGIC_VECTOR(31 downto 0));
 end;
 
 architecture behave of regmemwb is
+  signal writereg:         STD_LOGIC_VECTOR(4 downto 0);
   signal readdata, aluout: STD_LOGIC_VECTOR(31 downto 0);
 begin
   process(clk) begin
     if rising_edge(clk) then
+      writereg_wb <= writereg;
       readdata_wb <= readdata;
       aluout_wb   <= aluout;
+      writereg    <= writereg_mem;
       readdata    <= readdata_mem;
       aluout      <= aluout_mem;
     end if;
